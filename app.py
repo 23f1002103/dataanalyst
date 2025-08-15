17530ac976dea495799e57131862bca04015cc63
pruthvi280 <pruthviprasad280@gmail.com>
2025-08-15 13:55:48 +0530

    Your changes

--- app.py
+++ app.py
@@ -714,4 +714,6 @@ async def analyze_get_info():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
